a02624992dd00d3e318d23c432e9a4c8b50be39e
Margus Pärt <margus@wave.ee>
2014-11-13 12:07:32 +0200

    Adding cart products, price,

--- spec/CartSpec.js
+++ spec/CartSpec.js
@@ -1,13 +1,75 @@
 describe("Cart", function() {
   var cart;
+  var product1;
+  var product2;
 
   beforeEach(function() {
     cart = new Cart();
+    product1 = new Product({name: "Vesi", price: 1.5});
+    product2 = new Product({name: "Shiski", price: 20.0});
+    product3 = new Product({name: "Kartul", price: 20.0, unit: "kilograms"});
   });
 
   describe("total amount", function() {
+
     it("is zero for empty cart", function() {
       expect(cart.getTotalAmount()).toEqual(0.0);
     });
+
+    it("returns sum for cart with 2 items", function() {
+      cart.addProduct(product1, 1);
+      cart.addProduct(product2, 1);
+      expect(cart.getTotalAmount()).toEqual(21.5);
+    });
+
+    it("returns correct amount for multiple items of same product", function() {
+      cart.addProduct(new Product({price: 10.0}), 10);
+      expect(cart.getTotalAmount()).toEqual(100.0);
+    });
+
   });
+
+
+  describe("receipt", function() {
+
+    it("contains product name, quantity, price=total price", function() {
+      cart.addProduct(product1, 1);
+      cart.addProduct(product2, 10);
+      expect(cart.getReceipt()).toEqual("Vesi\t1 x 1.5 = 1.5\nShiski\t10 x 20 = 200\n");
+    });
+
+    it("shows three places after comma for items that have kg", function() {
+      cart.addProduct(product1, 1);
+      cart.addProduct(product3, 1.5555);
+      expect(cart.getReceipt()).toEqual("Vesi\t1 x 1.5 = 1.5\nKartul\t1.556 x 20 = 31.11\n");
+    });
+
+    it("contains grouped items (even if added multiple times)", function() {
+      cart.addProduct(product1, 1);
+      cart.addProduct(product1, 1);
+      cart.addProduct(product3, 2.5555);
+      cart.addProduct(product3, 2.5555);
+      expect(cart.getReceipt()).toEqual("Vesi\t2 x 1.5 = 3\nKartul\t5.111 x 20 = 102.22\n");
+    });
+
+  });
+
+  describe("automatic product addition", function() {
+
+    it("adds connected products automatically", function() {
+      var automaticallyAddedProduct = new Product();
+      var productWithAddition = new Product();
+
+      productWithAddition.getAutomaticallyAddedProducts = function() {
+        return [automaticallyAddedProduct];
+      }
+
+      cart.addProduct(productWithAddition, 2);
+
+      expect(cart.items).toContain({product: automaticallyAddedProduct, quantity: 2});
+    });
+
+  });
+
 });
+
--- src/Cart.js
+++ src/Cart.js
@@ -1,15 +1,66 @@
+function Product(opts) {
+  opts = opts || {};
+  this.name = opts.name;
+  this.price = opts.price;
+  this.unit = opts.unit;
+}
+
+
+
+Product.prototype.getName = function() {
+  return name;
+};
+
+Product.prototype.getAutomaticallyAddedProducts = function() {
+  return [];
+}
+
+
 function Cart() {
   this.items = [];
 }
 
 Cart.prototype.getTotalAmount = function() {
-  return 0;
+  var sum = 0;
+  for (var i in this.items) {
+    var price = this.items[i]["product"].price * this.items[i]["quantity"];
+    sum += price;
+  }
+  return sum;
 };
 
-Cart.prototype.add = function(amount) {
-  this.items.push(amount);
+Cart.prototype.addProduct = function(product, quantity) {
+  var existingItem = this.items.filter(function(item) {
+    return item.product==product;
+  })[0];
+
+  if(existingItem) {
+    existingItem.quantity += quantity;
+  } else {
+    this.items.push({product: product, quantity: quantity});
+  }
+
+  var automaticallyAddedProducts = product.getAutomaticallyAddedProducts();
+  if(automaticallyAddedProducts) {
+    var self = this;
+    automaticallyAddedProducts.forEach(function(automaticallyAddedProduct) {
+      self.addProduct(automaticallyAddedProduct, quantity);
+    });
+  }
 };
 
 Cart.prototype.getReceipt = function() {
-  return "receipt";
+  var receipt = "";
+  this.items.forEach(function(item) {
+    receipt += item.product.name;
+    receipt += "\t";
+    if (item.product.unit=="kilograms") {
+      receipt += item.quantity.toFixed(3) + " x " + item.product.price + " = " + (item.quantity*item.product.price).toFixed(2);
+    } else {
+      receipt += item.quantity + " x " + item.product.price + " = " + item.quantity*item.product.price;
+    }
+
+    receipt += "\n";
+  });
+  return receipt;
 };
